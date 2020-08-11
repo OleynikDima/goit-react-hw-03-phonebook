d752880b305abd18c8ed1c1965cd2d2186d173e9
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-08-11 11:20:02 +0300

    Updates

---
+++ precache-manifest.95e29212c4459196e86d3d92ea7a4731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a326fdb92d9886334a3ce300504f35",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "2956ab936d9e1cedde0f",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.6e73d5c6.chunk.css"
+  },
+  {
+    "revision": "2cd21712039f6c0f71b4",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.7dcbedb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.7dcbedb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2956ab936d9e1cedde0f",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.d25da414.chunk.js"
+  },
+  {
+    "revision": "00f9446489608817939d",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.46589c04.js"
+  }
+]);
